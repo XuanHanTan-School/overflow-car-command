de2fc581562d220981feea8a7f26e66910086184
Xuan Han Tan <187362236+XuanHanTan-School@users.noreply.github.com>
2024-12-29 21:48:11 +0800

    Updated validation for command websocket

--- main.py
+++ main.py
@@ -25,29 +25,67 @@ RTSP_OPTIONS = {
 def generate_error_string(message):
     return json.dumps({"type": "error", "message": message})
 
+def is_int(value):
+    try:
+        int(value)
+    except ValueError:
+        return False
+    
+    return True
+
+def is_bool(value):
+    return value is True or value is False
+
 async def handle_message(websocket):
     try:
         async for message in websocket:
-            message_data = json.loads(message)
-            if "token" not in message_data:
-                await websocket.send(generate_error_string("Token not provided."))
-                continue
+            try:
+                message_data = json.loads(message)
+                if "token" not in message_data:
+                    await websocket.send(generate_error_string("Token not provided."))
+                    continue
 
-            if message_data["token"] != PASSWORD:
-                await websocket.send(generate_error_string("Invalid token provided."))
-                continue
+                if message_data["token"] != PASSWORD:
+                    await websocket.send(generate_error_string("Invalid token provided."))
+                    continue
 
-            message_type = message_data["type"]
-            if message_type == "ping":
-                await websocket.send(json.dumps({"type": "pong"}))
-            elif message_type == "drive":
-                await drive_control.send(json.dumps({
-                    "angle": message_data["angle"],
-                    "forward": message_data["forward"],
-                    "accelerate": message_data["accelerate"]
-                }))
-            else:
-                await websocket.send(generate_error_string("Invalid message type"))
+                if "type" not in message_data:
+                    await websocket.send(generate_error_string("Message type not provided."))
+                    continue
+                
+                message_type = message_data["type"]
+                if message_type == "ping":
+                    await websocket.send(json.dumps({"type": "pong"}))
+                elif message_type == "drive":
+                    if "angle" not in message_data:
+                        await websocket.send(generate_error_string("Angle not provided."))
+                        continue
+                    if "forward" not in message_data:
+                        await websocket.send(generate_error_string("Forward (direction) not provided."))
+                        continue
+                    if "accelerate" not in message_data:
+                        await websocket.send(generate_error_string("Accelerate (pedal pressed) not provided."))
+                        continue
+
+                    if not is_int(message_data["angle"]) or not (-90 <= message_data["angle"] <= 90):
+                        await websocket.send(generate_error_string("Invalid angle provided."))
+                        continue
+                    if not is_bool(message_data["forward"]):
+                        await websocket.send(generate_error_string("Invalid forward (direction) provided."))
+                        continue
+                    if not is_bool(message_data["accelerate"]):
+                        await websocket.send(generate_error_string("Invalid accelerate (pedal pressed) provided."))
+                        continue
+
+                    await drive_control.send(json.dumps({
+                        "angle": message_data["angle"],
+                        "forward": message_data["forward"],
+                        "accelerate": message_data["accelerate"]
+                    }))
+                else:
+                    await websocket.send(generate_error_string("Invalid message type."))
+            except:
+                await websocket.send(generate_error_string("Failed to parse message."))
     except ConnectionClosedError:
         print("Connection to app closed unexpectedly.")
 
